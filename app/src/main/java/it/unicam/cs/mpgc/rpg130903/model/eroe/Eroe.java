53c5bf51f98f150563ac2cdf53a81ead00be9e87
Matilda De Carolis <matilda.decarolis@studenti.unicam.it>
2026-07-04 16:53:45 +0200

    Implementato l'aspetto del levelup e dello stipendio

--- app/src/main/java/it/unicam/cs/mpgc/rpg130903/model/eroe/Eroe.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg130903/model/eroe/Eroe.java
@@ -1,16 +1,31 @@
 package it.unicam.cs.mpgc.rpg130903.model.eroe;
 
 public class Eroe {
-    private String nome;
+    private final String nome;
     private StatoEroe statocorrente;
+    private int livello;
+    private int stipendio;
+    private int missionicompletate;
+
 
     public Eroe(String nome){
         this.nome = nome;
         this.statocorrente = new StatoPronto();
+        this.livello = 1;
+        this.missionicompletate = 0;
+        this.calcolaStipendio();
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public int getLivello() {
+        return livello;
+    }
+
+    public int getStipendio() {
+        return stipendio;
     }
 
     public void setStato(StatoEroe NuovoStato){
@@ -24,7 +39,28 @@ public class Eroe {
     public void riposo(){
         statocorrente.riposo(this);
     }
+
     public void inmissione(){
         statocorrente.inmissione((this));
+    }
+
+    public void aggiungiMissioneCompletata() {
+        this.missionicompletate++;
+        controllaLevelUp();
+    }
+
+    public void controllaLevelUp(){
+        int costantelivello = 1;
+        if (this.missionicompletate >= (this.livello + costantelivello)){
+            this.livello++;
+            this.missionicompletate = 0;
+            calcolaStipendio();
+            System.out.println("Congratulazioni " + this.nome + " è ora al livello" + this.livello + " e il suo stipendio è aumentato. Nuovo Stipendio" + this.stipendio);
+        }
+    }
+
+    public void calcolaStipendio(){
+        int costantestipendio = 10;
+        this.stipendio = this.livello * costantestipendio;
     }
 }
